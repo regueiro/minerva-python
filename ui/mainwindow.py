35009f42693e0a4ceed8f5b69a99b4ecdd5c010c
regueiro <santi@regueiro.es>
2013-02-01 20:17:06 +0100

    Divided interface in movie and tv show windows

--- ui/mainwindow.py
+++ ui/mainwindow.py
@@ -8,7 +8,7 @@ import os
 from PySide import QtGui
 from PySide import QtCore
 
-from mediamanager.manager import FolderManager
+from mediamanager.manager import FolderManager, LocalArtworkManager
 from ui.ui_mainwindow import Ui_MainWindow
 
 
@@ -17,7 +17,31 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
         self.actionScan.triggered.connect(self.load_shows)
+        self.actionScanMovies.triggered.connect(self.load_movies)
+        self.actionMovies.triggered.connect(self.change_to_movies)
+        self.actionTvShows.triggered.connect(self.change_to_shows)
         self.set_images()
+        self.change_to_movies()
+
+    def change_to_shows(self):
+        self.stackedWidget.setCurrentIndex(0)
+
+        self.actionMovies.setVisible(True)
+        self.actionScanMovies.setVisible(False)
+
+        self.actionTvShows.setVisible(False)
+        self.actionScan.setVisible(True)
+
+    def change_to_movies(self):
+        self.stackedWidget.setCurrentIndex(1)
+
+        self.actionTvShows.setVisible(True)
+        self.actionScan.setVisible(False)
+
+        self.actionMovies.setVisible(False)
+        self.actionScanMovies.setVisible(True)
+
+
 
     def load_shows(self):
 
@@ -30,18 +54,45 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
 #        contents = ShowList(showlist)
 
         folder_manager = FolderManager()
+        folder_manager.add_tv_folder(r"R:\Series")
+        folder_manager.add_tv_folder(r"S:\Series")
+        folder_manager.scan_tv_folders()
+        showlist = folder_manager.tv_show_list
+
+
+        art_manager = LocalArtworkManager()
+        for show in showlist:
+            art_manager.read_local_artwork(show)
+
+        contents = ShowList(showlist)
+
+        self.listView_2.setModel(contents)
+
+        self.show_selection_model = QtGui.QItemSelectionModel(contents, self.listView_2)
+        self.listView_2.setSelectionModel(self.show_selection_model)
+
+
+        self.show_selection_model.currentChanged.connect(self.change_images)
+
+    def load_movies(self):
+
+        folder_manager = FolderManager()
         folder_manager.add_movie_folder(r"P:\Peliculas")
         folder_manager.add_movie_folder(r"R:\Peliculas")
         folder_manager.add_movie_folder(r"H:\Peliculas")
         folder_manager.scan_movie_folders()
-        showlist = folder_manager.movie_list
-        contents = ShowList(showlist)
+        movie_list = folder_manager.movie_list
+        contents = ShowList(movie_list)
 
-        self.listView_2.setModel(contents)
-        self.listView_2.clicked.connect(self.change_images)
+        art_manager = LocalArtworkManager()
+        for movie in movie_list:
+            art_manager.find_local_movie_artwork(movie)
+
+        self.movieList.setModel(contents)
+        self.movieList.clicked.connect(self.change_movie_images)
 
     def set_images(self):
-        os.chdir(r"C:\Users\santi.REGUEIRO\Desarrollo\Python\Minerva")
+        os.chdir(r"C:\Users\santi.REGUEIRO\Desarrollo\Python\Minerva\src")
         self.bannerImage.setPixmap(QtGui.QPixmap("images/banner.jpg"))
         self.seasonBannerImage.setPixmap(QtGui.QPixmap("images/banner.jpg"))
 
@@ -63,21 +114,49 @@ class MainWindow(QtGui.QMainWindow, Ui_MainWindow):
 
         if show.art.banner:
             self.bannerImage.setPixmap(QtGui.QPixmap(show.art.banner))
+        else:
+            self.bannerImage.setPixmap(None)
         if show.art.banner:
             self.seasonBannerImage.setPixmap(QtGui.QPixmap(show.art.banner))
+        else:
+            self.seasonBannerImage.setPixmap(None)
 
         if show.art.fanart:
             self.fanartImage.setPixmap(QtGui.QPixmap(show.art.fanart))
+        else:
+            self.fanartImage.setPixmap(None)
         if show.art.poster:
             self.posterImage.setPixmap(QtGui.QPixmap(show.art.poster))
+        else:
+            self.posterImage.setPixmap(None)
         if show.art.season_poster:
             try:
                 self.seasonPosterImage.setPixmap(QtGui.QPixmap(show.art.season_poster['1']))
             except KeyError:
                 pass
+        else:
+            self.seasonPosterImage.setPixmap(None)
+
+    def change_movie_images(self,index):
+
+        movie = self.movieList.model().show(index)
+
+        if movie.art.banner:
+            self.movieOverviewBanner.setPixmap(QtGui.QPixmap(movie.art.banner))
+        else:
+            self.movieOverviewBanner.setPixmap(None)
+        if movie.art.fanart:
+            self.movieOverviewFanart.setPixmap(QtGui.QPixmap(movie.art.fanart))
+        else:
+            self.movieOverviewFanart.setPixmap(None)
+        if movie.art.poster:
+            self.movieOverviewPoster.setPixmap(QtGui.QPixmap(movie.art.poster))
+        else:
+            self.movieOverviewPoster.setPixmap(None)
 
     def resizeEvent(self, *args, **kwargs):
         self.window().setWindowTitle(str(self.size()))
+
 
 class ShowList(QtCore.QAbstractListModel):
     def __init__(self, contents):
@@ -93,3 +172,4 @@ class ShowList(QtCore.QAbstractListModel):
 
     def show(self, index):
         return self.contents[index.row()]
+
